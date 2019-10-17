7c94ee1cfdde2c6f8a22b76cefc01415a151f279
wangtao <649969462@qq.com>
2019-10-17 19:31:08 +0800

    add 	binary search 相关变形及 unittest

--- Python/searches/binary_search_variants.py
+++ Python/searches/binary_search_variants.py
@@ -59,3 +59,53 @@ def binary_search_right(sorted_collection: List[int], item: int) -> int:
             else:
                 left = midpoint + 1
     return -1
+
+
+def binary_search_left_not_less(sorted_collection: List[int], item: int) -> int:
+    """
+    二分查找
+
+    查找第一个大于等于给定值的元素
+    :param sorted_collection: 有序数组
+    :param item: 要查找的数据项
+    :return: index or -1
+    """
+    left = 0
+    right = len(sorted_collection) - 1
+    while left <= right:
+        midpoint = (left + right) // 2
+        current_item = sorted_collection[midpoint]
+        if current_item < item:
+            left = midpoint + 1
+        else:
+            if midpoint == 0 or sorted_collection[midpoint - 1] < current_item:
+                return midpoint
+            else:
+                right = midpoint - 1
+    return -1
+
+
+def binary_search_right_not_greater(sorted_collection: List[int], item: int) -> int:
+    """
+    二分查找
+
+    查找最后一个小于等于给定值的元素
+    :param sorted_collection: 有序数组
+    :param item: 要查找数据项
+    :return: index or -1
+    """
+    left = 0
+    right = len(sorted_collection) - 1
+    while left <= right:
+        midpoint = (left + right) // 2
+        current_item = sorted_collection[midpoint]
+        if current_item > item:
+            right = midpoint - 1
+        else:
+            if midpoint == len(sorted_collection) - 1 or sorted_collection[midpoint + 1] > item:
+                return midpoint
+            else:
+                left = midpoint + 1
+    return -1
+
+
--- Python/test/searches/test_binary_search_variants.py
+++ Python/test/searches/test_binary_search_variants.py
@@ -6,13 +6,21 @@ from Python.searches import binary_search_variants
 class TestBinarySearch(unittest.TestCase):
     def test_binary_search_left(self):
         res = binary_search_variants.binary_search_left([1, 2, 3, 3, 4, 4, 4, 5], 41)
-        print(res)
         self.assertEqual(-1, res)
 
     def test_binary_search_right(self):
         res = binary_search_variants.binary_search_right([1, 2, 3, 3, 4, 4, 4, 5, 5], 9)
-        print(res)
         self.assertEqual(-1, res)
+
+    def test_binary_search_not_less(self):
+        res = binary_search_variants.binary_search_left_not_less([1, 2, 2, 3, 3, 4, 4, 4, 5], 5)
+        self.assertEqual(8, res)
+
+    def test_binary_search_right_not_greater(self):
+        sorted_collection = [1, 2, 2, 4, 4, 4, 5]
+        search_item = 3
+        res = binary_search_variants.binary_search_right_not_greater(sorted_collection, search_item)
+        self.assertEqual(3, res)
 
 
 if __name__ == '__main__':
